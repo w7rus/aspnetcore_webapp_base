9701047172aafa1e73afad54a7c72333599c8b06
W7RUS-LAPTOP\w7rus <bragingrigory@gmail.com>
2022-07-09 11:45:29 +0700

    Add UserProfileController Add UserProfileHandler (In-Progress)

--- API/Controllers/UserProfileController.cs
+++ API/Controllers/UserProfileController.cs
@@ -1,9 +1,18 @@
-﻿using API.Controllers.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using API.Controllers.Base;
 using BLL.Handlers;
 using BLL.Services.Advanced;
+using DTO.Models.Generic;
+using DTO.Models.PermissionValue;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using Common.Models;
+using DTO.Models.UserProfile;
 
 namespace API.Controllers;
 
@@ -35,7 +44,53 @@ public class UserProfileController : CustomControllerBase
 
     #region Methods
 
-    //TODO: Endpoints
+    [HttpPost]
+    [Route(nameof(Create))]
+    [Authorize(AuthenticationSchemes = AuthenticationSchemes.JsonWebToken)]
+    [ProducesResponseType(typeof(UserProfileCreateResultDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Create(
+        [Required] [FromBody] UserProfileCreateDto data,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return ResponseWith(await _userProfileHandler.Create(data, cancellationToken));
+    }
+
+    [HttpGet]
+    [Route(nameof(Read))]
+    [Authorize(AuthenticationSchemes = AuthenticationSchemes.JsonWebToken)]
+    [ProducesResponseType(typeof(UserProfileReadResultDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Read(
+        [Required] [FromQuery] UserProfileReadDto data,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return ResponseWith(await _userProfileHandler.Read(data, cancellationToken));
+    }
+
+    [HttpPut]
+    [Route(nameof(Update))]
+    [Authorize(AuthenticationSchemes = AuthenticationSchemes.JsonWebToken)]
+    [ProducesResponseType(typeof(UserProfileUpdateResultDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Update(
+        [Required] [FromBody] UserProfileUpdateDto data,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return ResponseWith(await _userProfileHandler.Update(data, cancellationToken));
+    }
+
+    [HttpDelete]
+    [Route(nameof(Delete))]
+    [Authorize(AuthenticationSchemes = AuthenticationSchemes.JsonWebToken)]
+    [ProducesResponseType(typeof(OkResultDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Delete(
+        [Required] [FromQuery] UserProfileDeleteDto data,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return ResponseWith(await _userProfileHandler.Delete(data, cancellationToken));
+    }
 
     #endregion
 }
--- BLL/Handlers/UserProfileHandler.cs
+++ BLL/Handlers/UserProfileHandler.cs
@@ -1,11 +1,65 @@
-﻿using BLL.Handlers.Base;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using BLL.Handlers.Base;
+using Common.Models.Base;
+using DAL.Data;
+using DTO.Models.UserProfile;
+using Microsoft.Extensions.Logging;
 
 namespace BLL.Handlers;
 
 public interface IUserProfileHandler
 {
+    Task<IDtoResultBase> Create(UserProfileCreateDto data, CancellationToken cancellationToken = default);
+    Task<IDtoResultBase> Read(UserProfileReadDto data, CancellationToken cancellationToken = default);
+    Task<IDtoResultBase> Update(UserProfileUpdateDto data, CancellationToken cancellationToken = default);
+    Task<IDtoResultBase> Delete(UserProfileDeleteDto data, CancellationToken cancellationToken = default);
 }
 
 public class UserProfileHandler : HandlerBase, IUserProfileHandler
 {
+    #region Fields
+
+    private readonly ILogger<HandlerBase> _logger;
+    private readonly IAppDbContextAction _appDbContextAction;
+    private readonly IMapper _mapper;
+
+    #endregion
+
+    #region Ctor
+
+    public UserProfileHandler(ILogger<HandlerBase> logger, IAppDbContextAction appDbContextAction, IMapper mapper)
+    {
+        _logger = logger;
+        _appDbContextAction = appDbContextAction;
+        _mapper = mapper;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public async Task<IDtoResultBase> Create(UserProfileCreateDto data, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IDtoResultBase> Read(UserProfileReadDto data, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IDtoResultBase> Update(UserProfileUpdateDto data, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IDtoResultBase> Delete(UserProfileDeleteDto data, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    #endregion
 }
